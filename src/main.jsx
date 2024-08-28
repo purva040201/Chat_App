6a8f2b9de529c085118a27396e80d46826191f43
purva040201 <79846958+purva040201@users.noreply.github.com>
2024-08-28 13:52:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import AppContextProvider from './context/AppContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-<BrowserRouter basename='/chat_app1'>
+<BrowserRouter basename='/Chat_App'>
 <AppContextProvider>
 <App />
 </AppContextProvider>
